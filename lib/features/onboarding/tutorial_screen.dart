d14fe00e71d36d3ce8890c3eab6761298f37c6fb
Jun Park <ah.ho@hotmail.com>
2023-03-12 23:42:33 +1100

    tutorial page

---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/contants/gaps.dart';
+import 'package:tiktok_clone/contants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { firstPage, secondPage }
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.firstPage;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      //to the right
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      // to the left
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingPage = Page.secondPage;
+      });
+    } else {
+      _showingPage = Page.firstPage;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate, // when user is dragging
+      onPanEnd: _onPanEnd, //when finish the dragging
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  Text(
+                    "Watch cool vdieos!",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  )
+                ],
+              ),
+              secondChild: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  Text(
+                    "Follow the rules!",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Videos are personalized for you based on what you watch, like, and share.",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  )
+                ],
+              ),
+              crossFadeState: _showingPage == Page.firstPage
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: const Duration(
+                milliseconds: 300,
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          elevation: 0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size24,
+              horizontal: Sizes.size24,
+            ),
+            child: AnimatedOpacity(
+              opacity: _showingPage == Page.firstPage ? 0 : 1,
+              duration: const Duration(milliseconds: 300),
+              child: CupertinoButton(
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+                child: const Text("Enter the app!"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
