15265c1c7ebdaca6797c548f5d98774df9952523
Jun Park <ah.ho@hotmail.com>
2023-03-30 22:06:05 +1100

    finishing discover layout design and move on to inbox

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/contants/gaps.dart';
 import 'package:tiktok_clone/contants/sizes.dart';
 
 final tabs = [
@@ -11,18 +14,63 @@ final tabs = [
   "Brands",
 ];
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
+
+  @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen>
+    with SingleTickerProviderStateMixin {
+  final TextEditingController _textEditingController =
+      TextEditingController(text: "asdasdasdasd");
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(_dismissKeyboard);
+  }
+
+  void _dismissKeyboard() {
+    if (_tabController.index != 0) {
+      FocusScope.of(context).unfocus();
+    }
+  }
+
+  void _onSearchChange(String value) {
+    print(value);
+  }
+
+  void _onSaerchSubmitted(String value) {
+    print(value);
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    _tabController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           elevation: 1,
-          title: const Text("Discover"),
+          title: CupertinoSearchTextField(
+            controller: _textEditingController,
+            onChanged: _onSearchChange,
+            onSubmitted: _onSaerchSubmitted,
+          ),
           bottom: TabBar(
+            controller: _tabController,
             splashFactory: NoSplash.splashFactory,
             padding: const EdgeInsets.symmetric(
               horizontal: Sizes.size16,
@@ -44,8 +92,86 @@ class DiscoverScreen extends StatelessWidget {
           ), // need prefferedSizedWidget.
         ),
         body: TabBarView(
+          controller: _tabController,
           children: [
-            for (var tab in tabs)
+            GridView.builder(
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                padding: const EdgeInsets.all(
+                  Sizes.size6,
+                ),
+                itemCount: 20,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                  childAspectRatio: 9 / 20,
+                ),
+                itemBuilder: (context, index) => Column(
+                      children: [
+                        Container(
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(Sizes.size4),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 9 / 16, // give the ratio
+                            child: FadeInImage.assetNetwork(
+                              fit: BoxFit
+                                  .cover, // make it fit for the parent ratio.
+                              placeholder: "assets/image/view.jpg",
+                              image:
+                                  "https://images.unsplash.com/photo-1540483761890-a1f7be05d99f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80 ",
+                            ),
+                          ),
+                        ),
+                        Gaps.v10,
+                        const Text(
+                          "this is a very long caption for my tickto that im uplado just now heheheh",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: Sizes.size16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Gaps.v6,
+                        DefaultTextStyle(
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey.shade600,
+                          ),
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 12,
+                                backgroundImage: NetworkImage(
+                                  "https://images.unsplash.com/photo-1439434768192-c60615c1b3c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                                ),
+                              ),
+                              Gaps.h4,
+                              const Expanded(
+                                child: Text(
+                                  "my avatar is going to be very long",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size16,
+                                color: Colors.grey.shade600,
+                              ),
+                              Gaps.h2,
+                              const Text(
+                                "2.5M",
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+            for (var tab in tabs.skip(1))
               Center(
                 child: Text(
                   tab,
